5f189c1b3a35a0aedcf1f93e1dcf50633056b914
杨奇 <yangq1@hsit.com.cn>
2023-10-12 17:56:37 +0800

    初始化项目vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,22 @@
+import { defineUserConfig,defaultTheme } from 'vuepress'
+import { docsearchPlugin } from '@vuepress/plugin-docsearch'
+import { backToTopPlugin } from '@vuepress/plugin-back-to-top'
+
+export default defineUserConfig({
+  lang: 'zh-CN',
+  title: '前端书签',
+  description: '收集前端相关官网和工具的网址',
+  plugins: [
+    backToTopPlugin(),
+    docsearchPlugin({
+      // 配置项
+    }),
+  ],
+  theme: defaultTheme({
+    navbar: [
+      {  text: '前端文档', link: '/Document/' },
+      {  text: '前端工具', link: '/Tools/' },
+      {  text: '前端社区', link: '/Community/' },
+    ]
+  })
+})
