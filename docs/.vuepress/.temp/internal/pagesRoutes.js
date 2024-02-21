05d0179d1fa15e3e123de8c43f782dc1d74fbeb3
杨奇 <yangq1@hsit.com.cn>
2024-02-21 18:16:21 +0800

    增加全局搜索

--- docs/.vuepress/.temp/internal/pagesRoutes.js
+++ docs/.vuepress/.temp/internal/pagesRoutes.js
@@ -1,7 +1,7 @@
 export const pagesRoutes = [
   ["v-8daa1a0e","/",{"title":""},["/index.md"]],
-  ["v-a3b8710a","/Document/",{"title":"前端文档"},["/Document/index.md"]],
   ["v-d06e0d82","/Community/",{"title":"前端社区"},["/Community/index.md"]],
+  ["v-a3b8710a","/Document/",{"title":"前端文档"},["/Document/index.md"]],
   ["v-5f44720d","/Tools/",{"title":"前端工具"},["/Tools/index.md"]],
   ["v-3706649a","/404.html",{"title":""},[]],
 ]
